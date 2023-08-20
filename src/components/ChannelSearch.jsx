f8c1aebbeb7df697c2a943bafdb4c8d271b24b7e
kenliii <90875261+kenliii@users.noreply.github.com>
2023-08-20 12:17:52 -0700

    channel search

--- src/components/ChannelSearch.jsx
+++ src/components/ChannelSearch.jsx
@@ -1,8 +1,22 @@
-import React from 'react'
+import React,  {useStae, useEffect} from 'react'
+import {useChatContext} from 'stream-chat-react'
+
+import {SearchIcon} from '../assets'
 
 const ChannelSearch = () => {
   return (
-    <div>ChannelSearch</div>
+    <div className='channel-search__container'>
+        <div className='channel-search__input__wrapper'>
+            <div className='channel-search__input__icon'>
+                <SearchIcon />
+            </div>
+            <input 
+                className='channel-search__input__text' 
+                placeholder='search' 
+                type='text' value={} />
+        </div>
+
+    </div>
   )
 }
 

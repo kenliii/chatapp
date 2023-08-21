26f2abec822c64ee4a0398965e5a9cf68f6119a9
kenliii <90875261+kenliii@users.noreply.github.com>
2023-08-21 00:26:18 -0700

    chatbox working

--- src/components/ChannelSearch.jsx
+++ src/components/ChannelSearch.jsx
@@ -1,9 +1,28 @@
-import React,  {useStae, useEffect} from 'react'
+import React,  {useState, useEffect} from 'react'
 import {useChatContext} from 'stream-chat-react'
 
 import {SearchIcon} from '../assets'
 
 const ChannelSearch = () => {
+    const [query, setQuery] = useState('');
+    const [loading, setLoading] = useState(false);
+
+    const getChannels = async(text) => {
+        try {
+                //todo: fetch channels
+        } catch (error) {
+            setQuery('')
+        }
+    }
+    
+    const onSearch = (event) => {
+        event.preventDefault();
+
+        setLoading(true);
+        setQuery(event.target.value);
+        getChannels(event.target.value)
+    }
+
   return (
     <div className='channel-search__container'>
         <div className='channel-search__input__wrapper'>
@@ -13,7 +32,9 @@ const ChannelSearch = () => {
             <input 
                 className='channel-search__input__text' 
                 placeholder='search' 
-                type='text' value={} />
+                type='text' 
+                value={query}
+                onChange={onSearch} />
         </div>
 
     </div>

0e5c6d30f11bf7c314ea15e9abc628319dc82808
kenliii <90875261+kenliii@users.noreply.github.com>
2023-08-30 23:48:13 -0700

    working on userlist

--- src/components/CreateChannel.jsx
+++ src/components/CreateChannel.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useChatContext } from 'stream-chat-react'
 
 import {UserList} from './'
@@ -12,17 +12,26 @@ const ChannelNameInput = ({ channelName = '', setChannelName }) => {
   }
 
   return (
-    <div className='channel-name-input__wrapper'>
+    <div className="channel-name-input__wrapper">
       <p>Name</p>
-      <input value={channelName} onChange={handleChange} placeholder="channel-name (no spaces)" />
+      <input value={channelName} onChange={handleChange} placeholder="channel-name" />
       <p>Add Members</p>
     </div>
   )
 }
 
-const CreateChannel = () => {
+const CreateChannel = ({ createType, setIsCreating }) => {
+  const [channelName, setChannelName] = useState('');
+
   return (
-    <div>ChannelNameInput</div>
+    <div className='create-channel__container'>
+      <div className='create-channel__header'>
+        <p>{createType === 'team' ? 'Create a New Channel' : 'Send a Direct Message'}</p>
+        <CloseCreateChannel setIsCreating={setIsCreating} />
+      </div>
+      {createType === 'team' && <ChannelNameInput channelName={channelName} setChannelName={setChannelName}/>}
+      <UserList />
+    </div>
   )
 }
 
---
+++ src/components/UserList.jsx
@@ -0,0 +1,48 @@
+import React, { useEffect, useState } from "react";
+import { Avatar, useChatContext } from "stream-chat-react";
+
+import { InviteIcon } from "../assets";
+
+const ListContainer = ({ children }) => {
+  return (
+    <div className="user-list__container">
+      <div className="user-list__header">
+        <p>User</p>
+        <p>Invite</p>
+      </div>
+      {children}
+    </div>
+  );
+};
+
+const UserItem = () => {
+  return (
+    <div className="user-item__wrapper">
+      <Avatar />
+    </div>
+  );
+};
+
+const UserList = () => {
+  const { client } = useChatContext();
+  const [users, setUsers] = useState([]);
+  const [loading, setLoading] = useState([]);
+
+  useEffect(() => {
+    const getUsers = async () => {
+      if (loading) return;
+
+      setLoading(true);
+
+      try {
+        const response = await client.queryUsers({
+          id: { $ne: client.userID },
+        });
+      } catch (error) {}
+    };
+  }, []);
+
+  return <ListContainer>Userlist</ListContainer>;
+};
+
+export default UserList;
